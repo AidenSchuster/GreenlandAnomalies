4ac9fee5aaa4ca513deedceb8da22137ed742ad1
AidenSchuster <143426875+AidenSchuster@users.noreply.github.com>
2024-05-28 13:48:07 -0400

    Add files via upload

--- Plots_Greenland.m
+++ Plots_Greenland.m
@@ -9,40 +9,6 @@ daspect([1 aspect_ratio 1])
 plot(x_coast,y_coast,'r')
 hold off
 %%
-% Plot Script 
-%Plot Coast Line With extended area
-clf
-hold on
-plot(cx,cy,'k')
-scatter(lon,lat,3,'MarkerFaceColor','g','MarkerEdgeAlpha','.05')
-xlim([-80,-35])
-ylim([55,80])
-daspect([1 aspect_ratio 1])
-plot(Southgcoast(:,1),Southgcoast(:,2),'r')
-plot(Southgcoast(:,1),y5_Scoast,'r')
-plot(Southgcoast(:,1),extendedcoast_S,'--b')
-plot(Westgcoast(:,1),Westgcoast(:,2),'r')
-plot(x5_Wcoast,Westgcoast(:,2),'r')
-plot(extendedcoast_W,Westgcoast(:,2),'--b')
-plot(Eastgcoast(:,1),Eastgcoast(:,2),'r')
-plot(x5_Ecoast,Eastgcoast(:,2),'r')
-plot(extendedcoast_E,Eastgcoast(:,2),'--b')
-plot(Gap_plus(:,1),Gap_plus(:,2), 'r')
-plot(Gap_minus(:,1),Gap_minus(:,2), 'r')
-hold off
-%% Plot Section on Line 131
-clf
-hold on 
-plot(x_Wcoast,y_SWcoast, 'r')
-plot(x5_Wcoast,y_SWcoast,'r')
-plot(cx,cy, 'k')
-xlim([-60,-45])
-ylim([60,70])
-scatter(W_lon_coast,W_lat_coast,1,'MarkerFaceColor','b','MarkerEdgeAlpha','0') ;
-scatter(W_lon,W_lat,1,'MarkerFaceColor','r','MarkerEdgeAlpha','0') ;
-hold off
-clf
-%%
 %Plot Angled Rectangles
 clf
 hold on
@@ -53,28 +19,6 @@ xlim([-60,-45])
 ylim([60,70])
 plot(x_Wcoast,y_SWcoast, 'r')
 plot(x5_Wcoast,y_SWcoast,'r')
-hold off
-%% Plot bodged coastlines
-clf
-hold on
-plot(cx,cy,'k')
-daspect([1 aspect_ratio 1])
-scatter(lon,lat,3,'MarkerFaceColor','g','MarkerEdgeAlpha','.05')
-xlim([-80,-35])
-ylim([55,80])
-plot(x_coast,y_coast,'b')
-scatter(off_x,off_y,'r')
-hold off
-%%  Plot bodged coastlines (not selected)
-clf
-hold on
-plot(cx,cy,'k')
-daspect([1 aspect_ratio 1])
-xlim([-80,-35])
-ylim([55,80])
-plot(x_coast,y_coast,'b')
-scatter(exten(:,1),exten(:,2),'r')
-scatter(exten2(:,1),exten2(:,2),'b')
 hold off
 %%
 % Test of Inpolygon
@@ -90,19 +34,7 @@ scatter(exten_plus2(~in_plus2,1),exten_plus2(~in_plus2,2),'r')
 scatter(exten_minus(~in_minus,1),exten_minus(~in_minus,2),'r')
 scatter(exten_minus(~in_minus2,1),exten_minus2(~in_minus2,2),'r')
 hold off
-%% Plot all inverse lines
-clf
-hold on
-plot(cx,cy,'k')
-daspect([1 aspect_ratio 1])
-xlim([-80,-35])
-ylim([55,80])
-plot(x_coast,y_coast,'b')
-for i = 1:1:length(x_perp_W)
-    scatter(x_perp_W{i},y_perp_W{i},'r')
-end
-hold off
-%% Final Extended Coast plot (hopefully)
+%% Final Extended Coast plot
 clf
 hold on
 plot(cx,cy,'k')
@@ -111,4 +43,17 @@ daspect([1 aspect_ratio 1])
 xlim([-80,-35])
 ylim([55,80])
 plot(off_coast(:,1),off_coast(:,2),'r')
+scatter(reff_x,reff_y,'g')
+hold off
+%% Plot restricted Coastal Casts
+clf
+hold on
+plot(cx,cy,'k')
+plot(x_coast,y_coast,'b')
+daspect([1 aspect_ratio 1])
+xlim([-80,-35])
+ylim([55,80])
+plot(off_coast(:,1),off_coast(:,2),'r')
+scatter(lon(in),lat(in),3,'MarkerFaceColor','g','MarkerEdgeAlpha','.05')
+plot(combined_x,combined_y,'r')
 hold off
