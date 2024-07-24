0d3e937ba56f0e83e0c6882be8858b1e4428ee65
AidenSchuster <143426875+AidenSchuster@users.noreply.github.com>
2024-07-24 13:39:26 -0400

    Add files via upload

---
+++ readNODC_Aiden01.m
@@ -0,0 +1,168 @@
+% Make sure to extract tar.gz files to .nc before running
+clear all
+close all
+
+
+
+cd C:\Users\ajs82292\Desktop\Research\Matlab\Source\nodc_cleaning
+
+direc=ls;
+direc(1:2,:)=[];
+
+BB=1;
+for DD=1:size(direc,1)
+        
+    ld=['cd ' direc(DD,:)];
+    eval(ld) ;
+
+
+name=ls ;
+name(1:2,:)=[];
+
+for j=1:size(name,1)
+    if mod(j,50)==0
+        % display([num2str(DD) '   ' num2str(size(direc,1)) '    '  num2str(j) '   ' num2str(size(name,1))]) 
+    end
+    
+ nh=[name(j,:)];
+
+try % addded
+ lat{BB}=ncread(nh,'lat'); % added (BB,:) changed to {BB}
+ lon{BB}=ncread(nh,'lon');
+ date=double(ncread(nh,'date'));
+ temp{BB}=ncread(nh,'Temperature');
+ sal{BB}=ncread(nh,'Salinity');
+ depth{BB}=ncread(nh,'z');
+ uba=num2str(date);
+ year{BB}=str2num(uba(1:4));
+ month{BB}=str2num(uba(5:6));
+ day{BB}=str2num(uba(7:8));
+ 
+BB=BB+1;
+
+catch ME
+        warning('Skipped file %s due to missing variable: %s', nh, ME.message);
+        continue;
+
+end
+end
+
+cd ..
+
+end
+cd ..
+
+whos lat lon temp sal depth year month day
+
+% convert lat lon year mon day into doubles instead of cells
+lat = cell2mat(lat) ;
+lon = cell2mat(lon) ;
+year = cell2mat(year) ;
+month = cell2mat(month) ;
+day = cell2mat(day) ;
+
+save('C:\Users\ajs82292\Desktop\Research\Matlab\Source\nodc_cleaning\01Greenland_temporary.mat','lat','lon','temp','sal','depth','year','month','day')
+
+
+
+% checking if there are profiles with depths 'inverted'. Also,
+% eliminate profiles with less than two points
+cont=1;
+for j=1:size(depth,2)
+    dt=depth{j};
+    iit=min(find(diff(dt)<0));
+    if isempty(iit)  & length(dt)>1 
+        ind(cont)=j;
+        cont=cont+1;
+    end
+end
+
+whos year month day lon lat depth temp sal ind
+
+year=year(ind);
+month=month(ind);
+day=day(ind);
+lon=lon(ind);
+lat=lat(ind);
+depth=depth(ind);
+temp=temp(ind);
+sal=sal(ind);
+
+dept=depth;
+sals=sal;
+temt=temp;
+
+clear depth sal temp
+
+whos year month day lon lat dept temt sals ind
+clear ind cont ds dt iis iit j ss tt
+
+
+% now, need to select only profiles where both T and S exist
+clear ind
+cont=1;
+for j=1:size(sals,2)
+    if mod(j,1)==1000;display([j size(sals,2)])
+    end
+t=temt{j};
+s=sals{j};
+
+    if sum(isfinite(t))==sum(isfinite(s))
+        ind(cont)=j;
+        cont=cont+1;
+    end
+end
+
+whos year month day lon lat dept temt sals ind
+
+year=year(ind);
+month=month(ind);
+day=day(ind);
+lon=lon(ind);
+lat=lat(ind);
+dept=dept(ind);
+temt=temt(ind);
+sals=sals(ind);
+
+
+whos year month day lon lat dept temt sals ind
+clear ind cont ds dt iis iit j ss tt
+
+
+
+for j=1:size(dept,2)
+    watdept(j)=max(dept{j});
+end
+
+
+
+
+
+ind=find(isfinite(watdept));
+year=year(ind);
+month=month(ind);
+day=day(ind);
+lon=lon(ind);
+lat=lat(ind);
+dept=dept(ind);
+temt=temt(ind);
+sals=sals(ind);
+watdept=watdept(ind);
+
+
+
+whos  year month day lon lat dept temt sals ind watdept 
+clear ind cont
+
+
+
+watdep=watdept;
+dep=dept;
+temp=temt;
+sal=sals;
+
+yea=year;
+mon=month;
+
+whos yea mon day lon lat dep watdep temp sal
+save('C:\Users\ajs82292\Desktop\Research\Matlab\Source\nodc_files_data\01GREENLAND_temp_sal','yea','mon','day','lon','lat','dep','watdep','temp','sal')
